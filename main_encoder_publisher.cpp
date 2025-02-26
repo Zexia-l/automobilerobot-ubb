1ad1c18e19f89cfefee99eb169cebd34d7998b4f
Zexia <denischool1@gmail.com>
2025-02-26 20:59:09 +0700

    adding new odometry program

--- main_encoder_publisher.cpp
+++ main_encoder_publisher.cpp
@@ -6,6 +6,21 @@
 #include <rclc/executor.h>
 
 #include <std_msgs/msg/int32_multi_array.h>  // Menggunakan array untuk mengirim data encoder
+/*
+#include <tf2_msgs/msg/tf_message.h>
+#include <math.h>
+*/
+#include <nav_msgs/msg/odometry.h>
+#include <geometry_msgs/msg/transform_stamped.h>
+#include <geometry_msgs/msg/quaternion.h>
+
+
+const int motorCount = 4;
+const float wheel_radius = 0; // radius roda dalam meter
+const float wheel_base_x = 0;  // jarak antara roda depan-belakang (m)
+const float wheel_base_y = 0;  // jarak antara roda kiri-kanan (m)
+const float encoder_resolution = 4096.0;
+const float gear_ratio = 1.0;
 
 // Pin definitions for motors
 const int R_EN[] = {22, 26, 30, 34};
@@ -20,10 +35,17 @@ const int speedValue = 150;
 const int delayTime = 2000;
 
 volatile long encoderCount[motorCount] = {0, 0, 0, 0};
+float x = 0.0, y = 0.0, theta = 0.0;
+float prev_encoder[motorCount] = {0, 0, 0, 0};
 
 // Micro-ROS variables
+/*
 rcl_publisher_t encoder_publisher;
 std_msgs__msg__Int32MultiArray encoder_msg;
+*/
+
+rcl_publisher_t odom_publisher;
+nav_msgs__msg__Odometry odom_msg;
 
 rclc_executor_t executor;
 rclc_support_t support;
@@ -33,6 +55,46 @@ rcl_timer_t timer;
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+void calculate_odometry() {
+  float d[motorCount];
+  for (int i = 0; i < motorCount; i++) {
+      d[i] = ((encoderCount[i] - prev_encoder[i]) / encoder_resolution) * (2 * M_PI * wheel_radius) / gear_ratio;
+      prev_encoder[i] = encoderCount[i];
+  }
+  
+  float dx = (d[0] - d[1] - d[2] + d[3]) / 4.0;
+  float dy = (-d[0] - d[1] + d[2] + d[3]) / 4.0;
+  float dtheta = (-d[0] + d[1] - d[2] + d[3]) / (4.0 * (wheel_base_x + wheel_base_y));
+  
+  float new_x = x + (dx * cos(theta) - dy * sin(theta));
+  float new_y = y + (dx * sin(theta) + dy * cos(theta));
+  float new_theta = theta + dtheta;
+  
+  x = new_x;
+  y = new_y;
+  theta = new_theta;
+  
+  odom_msg.pose.pose.position.x = x;
+  odom_msg.pose.pose.position.y = y;
+  odom_msg.pose.pose.position.z = 0.0;
+  
+  geometry_msgs__msg__Quaternion quat;
+  quat.x = 0.0;
+  quat.y = 0.0;
+  quat.z = sin(theta / 2.0);
+  quat.w = cos(theta / 2.0);
+  odom_msg.pose.pose.orientation = quat;
+  
+  rcl_publish(&odom_publisher, &odom_msg, NULL);
+}
+
+timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+      calculate_odometry();
+  }
+}
 
 // Error handling loop
 void error_loop() {
--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,105 @@
 #include <Arduino.h>
+#include <micro_ros_platformio.h>
 
-// Struktur untuk menyimpan konfigurasi motor
-struct Motor {
-  int RPWM;  // Pin PWM kanan
-  int LPWM;  // Pin PWM kiri
-  int REN;   // Pin Enable kanan
-  int LEN;   // Pin Enable kiri
-};
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
 
-// Konfigurasi semua motor
-Motor motors[] = {
-  {3, 2, 24, 22},  // FRONT LEFT
-  {5, 4, 28, 26},  // BACK LEFT
-  {6, 7, 30, 32},  // FRONT RIGHT
-  {8, 9, 34, 36}   // BACK RIGHT
-};
+#include <std_msgs/msg/int32_multi_array.h>
+#include <nav_msgs/msg/odometry.h>
+#include <tf2_msgs/msg/tf_message.h>
+#include <geometry_msgs/msg/transform_stamped.h>
+#include <geometry_msgs/msg/quaternion.h>
+#include <math.h>
 
-// Jumlah motor
-const int NUM_MOTORS = sizeof(motors) / sizeof(Motor);
+const int motorCount = 4;
+const float wheel_radius = 0.05; // radius roda dalam meter
+const float wheel_base_x = 0.2;  // jarak antara roda depan-belakang (m)
+const float wheel_base_y = 0.2;  // jarak antara roda kiri-kanan (m)
+const float encoder_resolution = 4096.0;
+const float gear_ratio = 1.0;
 
-// Fungsi untuk inisialisasi motor
-void initMotors() {
-  for (int i = 0; i < NUM_MOTORS; i++) {
-    pinMode(motors[i].RPWM, OUTPUT);
-    pinMode(motors[i].LPWM, OUTPUT);
-    pinMode(motors[i].REN, OUTPUT);
-    pinMode(motors[i].LEN, OUTPUT);
+volatile long encoderCount[motorCount] = {0, 0, 0, 0};
+float x = 0.0, y = 0.0, theta = 0.0;
+float prev_encoder[motorCount] = {0, 0, 0, 0};
 
-    // Aktifkan pin enable
-    digitalWrite(motors[i].REN, HIGH);
-    digitalWrite(motors[i].LEN, HIGH);
-  }
+// Micro-ROS variables
+rcl_publisher_t odom_publisher;
+nav_msgs__msg__Odometry odom_msg;
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+// Function to calculate odometry
+void calculate_odometry() {
+    float d[motorCount];
+    for (int i = 0; i < motorCount; i++) {
+        d[i] = ((encoderCount[i] - prev_encoder[i]) / encoder_resolution) * (2 * M_PI * wheel_radius) / gear_ratio;
+        prev_encoder[i] = encoderCount[i];
+    }
+    
+    float dx = (d[0] - d[1] - d[2] + d[3]) / 4.0;
+    float dy = (-d[0] - d[1] + d[2] + d[3]) / 4.0;
+    float dtheta = (-d[0] + d[1] - d[2] + d[3]) / (4.0 * (wheel_base_x + wheel_base_y));
+    
+    float new_x = x + (dx * cos(theta) - dy * sin(theta));
+    float new_y = y + (dx * sin(theta) + dy * cos(theta));
+    float new_theta = theta + dtheta;
+    
+    x = new_x;
+    y = new_y;
+    theta = new_theta;
+    
+    odom_msg.pose.pose.position.x = x;
+    odom_msg.pose.pose.position.y = y;
+    odom_msg.pose.pose.position.z = 0.0;
+    
+    geometry_msgs__msg__Quaternion quat;
+    quat.x = 0.0;
+    quat.y = 0.0;
+    quat.z = sin(theta / 2.0);
+    quat.w = cos(theta / 2.0);
+    odom_msg.pose.pose.orientation = quat;
+    
+    rcl_publish(&odom_publisher, &odom_msg, NULL);
 }
 
-// Fungsi untuk mengontrol satu motor
-void setMotor(Motor motor, int speed) {
-  if (speed > 0) {
-    analogWrite(motor.RPWM, speed); // Maju
-    analogWrite(motor.LPWM, 0);
-    digitalWrite(motor.REN, HIGH);  // Aktifkan enable
-    digitalWrite(motor.LEN, HIGH);
-  } else if (speed < 0) {
-    analogWrite(motor.RPWM, 0);
-    analogWrite(motor.LPWM, -speed); // Mundur
-    digitalWrite(motor.REN, HIGH);  // Aktifkan enable
-    digitalWrite(motor.LEN, HIGH);
-  } else {
-    analogWrite(motor.RPWM, 0); // Berhenti
-    analogWrite(motor.LPWM, 0);
-    digitalWrite(motor.REN, LOW);  // Aktifkan enable
-    digitalWrite(motor.LEN, LOW);
-  }
-}
-
-// Fungsi untuk mengontrol semua motor
-void setAllMotors(int speeds[]) {
-  for (int i = 0; i < NUM_MOTORS; i++) {
-    setMotor(motors[i], speeds[i]);
-  }
+// Timer callback for publishing odometry
+timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
+    RCLC_UNUSED(last_call_time);
+    if (timer != NULL) {
+        calculate_odometry();
+    }
 }
 
 void setup() {
-  // Inisialisasi komunikasi serial
-  Serial.begin(115200);
-  
-  // Inisialisasi semua motor
-  initMotors();
+    SerialUSB.begin(115200);
+    set_microros_serial_transports(SerialUSB);
+    delay(2000);
+    
+    allocator = rcl_get_default_allocator();
+    RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+    RCCHECK(rclc_node_init_default(&node, "odometry_node", "", &support));
+    
+    RCCHECK(rclc_publisher_init_default(
+        &odom_publisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(nav_msgs, msg, Odometry),
+        "odom"));
+    
+    const unsigned int timer_timeout = 100;
+    RCCHECK(rclc_timer_init_default(
+        &timer,
+        &support,
+        RCL_MS_TO_NS(timer_timeout), timer_callback));
+    
+    RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+    RCCHECK(rclc_executor_add_timer(&executor, &timer));
 }
 
 void loop() {
-  // Baca data PWM dari ROS
-  if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');
-    int speeds[NUM_MOTORS];
-
-    // Parsing data dari format "FL,BL,FR,BR\n"
-    sscanf(input.c_str(), "%d,%d,%d,%d", &speeds[0], &speeds[1], &speeds[2], &speeds[3]);
-
-    // Kontrol semua motor
-    setAllMotors(speeds);
-  }
+    RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
